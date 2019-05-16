a5f396b62ff3af9989f65390bb8bb08e10229fc7
LDorine <dodosav.fr@gmail.com>
2019-05-16 09:15:48 +0200

    debug

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route(name="blog_")
- */
+
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="index")
+     * @Route("/blog", name="blog_index")
      * @return Response
      */
     public function index() : Response
@@ -23,7 +21,7 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/show/{slug}", name="show",requirements={"slug"="^[a-z0-9]+(?:-[a-z0-9]+)*$"})
+     * @Route("/blog/show/{slug}", name="blog_show",requirements={"slug"="^[a-z0-9]+(?:-[a-z0-9]+)*$"})
      * @param $slug
      * @return Response
      */
