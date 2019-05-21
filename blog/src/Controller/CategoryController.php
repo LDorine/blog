6adf3af39a77964a501609238ef62466ad7b941f
LDorine <dodosav.fr@gmail.com>
2019-05-21 14:43:09 +0200

    Adding category with form

---
+++ blog/src/Controller/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="blog_add")
+     * @param Request $request
+     * @return Response
+     */
+    public function add(Request $request) : Response
+    {
+        $form = $this->createForm(CategoryType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $insert = $this->getDoctrine()->getManager();
+            $insert->persist($data);
+            $insert->flush();
+            return $this->render('blog/success.html.twig');
+        } else {
+            return $this->render(
+                'blog/form.html.twig',
+                ['form' => $form->createView()]
+            );
+        }
+    }
+}
